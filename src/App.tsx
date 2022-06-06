56f4cd90e83091df3eba77d6c6ea68dc854f74b6
sckimynwa <tsb03672@naver.com>
2022-06-06 20:17:48 +0900

    feat: add style packages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Editor from '@react-page/editor';
 
 import slate from '@react-page/plugins-slate';
 import image from '@react-page/plugins-image';
+import { Box } from '@mui/material';
 
 const cellPlugins = [slate(), image];
 
@@ -12,8 +13,12 @@ export default function SimpleExample() {
   const [value, setValue] = useState<Value | null>(null);
 
   return (
-    <div>
+    <Box
+      sx={{
+        padding: '32px 64px',
+      }}
+    >
       <Editor cellPlugins={cellPlugins} value={value} onChange={setValue} />
-    </div>
+    </Box>
   );
 }
